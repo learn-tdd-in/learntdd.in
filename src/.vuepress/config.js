eb87c6f099c83c37e39d6a0b389fa7d664dbdc4b
Josh Justice <me@codingitwrong.com>
2020-10-20 07:15:56 -0400

    Add GA

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -50,5 +50,11 @@ module.exports = {
   plugins: [
     '@vuepress/plugin-back-to-top',
     '@vuepress/plugin-medium-zoom',
+    [
+      '@vuepress/google-analytics',
+      {
+        'ga': 'UA-80822752-1',
+      },
+    ],
   ]
 }
