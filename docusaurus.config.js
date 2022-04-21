ab6741476ede3e8ba907755fa7c651352176916c
Josh Justice <me@codingitwrong.com>
2022-04-21 07:43:16 -0400

    Update footer

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -84,44 +84,14 @@ const config = {
         style: 'dark',
         links: [
           {
-            title: 'Docs',
+            title: 'More',
             items: [
               {
-                label: 'Tutorial',
-                to: '/docs/intro',
+                label: 'Contact',
+                to: 'mailto:tdd@codingitwrong.com',
               },
             ],
           },
-          {
-            title: 'Community',
-            items: [
-              {
-                label: 'Stack Overflow',
-                href: 'https://stackoverflow.com/questions/tagged/docusaurus',
-              },
-              {
-                label: 'Discord',
-                href: 'https://discordapp.com/invite/docusaurus',
-              },
-              {
-                label: 'Twitter',
-                href: 'https://twitter.com/docusaurus',
-              },
-            ],
-          },
-          // {
-          //   title: 'More',
-          //   items: [
-          //     {
-          //       label: 'Blog',
-          //       to: '/blog',
-          //     },
-          //     {
-          //       label: 'GitHub',
-          //       href: 'https://github.com/facebook/docusaurus',
-          //     },
-          //   ],
-          // },
         ],
         copyright: `Copyright © ${new Date().getFullYear()} Josh Justice.`,
       },
