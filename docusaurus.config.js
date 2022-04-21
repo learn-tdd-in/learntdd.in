4127be1aab6b4d14eabc08e473ad3ae2f54e26a0
Josh Justice <me@codingitwrong.com>
2022-04-21 08:18:48 -0400

    Add GA

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -21,6 +21,10 @@ const config = {
       'classic',
       /** @type {import('@docusaurus/preset-classic').Options} */
       ({
+        gtag: {
+          trackingID: 'G-D5WS3PECNF',
+          anonymizeIP: true,
+        },
         docs: {
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
